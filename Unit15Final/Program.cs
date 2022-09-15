bd1d56d55589bbf8f350fa83f8e1c1808b1d24e2
SDozH <soldatov_alex@bk.ru>
2022-09-15 22:56:42 +0300

    Добавьте файлы проекта.

---
+++ Unit15Final/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Unit15Final
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine("Имена всех студентов: \n{0}",string.Join("\n", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var result= from students in classes
+                        from student in students.Students
+                        select student;
+            return result.ToArray();
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
